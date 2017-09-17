6abdadf184783ae4e3977b253f432e0ee6302c70
raghavendra talur <raghavendra.talur@gmail.com>
2017-09-17 19:36:38 +0530

    more updates
    Signed-off-by: raghavendra talur <raghavendra.talur@gmail.com>

--- virt-net-delete.sh
+++ virt-net-delete.sh
@@ -1,1 +1,10 @@
-for each in `ls *.xml`; do echo $each ;  virsh net-update rtalur-default delete dns-host --xml $each  --live --config; done
+NET="rtalur_vagrant_single_dev obnox_vagrant_dev default vagrant-libvirt"
+
+for net in $NET
+do
+        for each in `ls *.xml`
+        do
+                echo $each
+                virsh net-update $net delete dns-host --xml $each  --live --config
+        done
+done
--- virt-net-update.sh
+++ virt-net-update.sh
@@ -7,7 +7,15 @@ NET="rtalur-default"
 for each in $VMS
 do
         mac_add=`virsh  dumpxml  $each | awk -F "=" '/mac address/ {print $2}' | awk -F "'" '{print $2}'`
-        ip_add=`awk "/${mac_add}/ {print \\$1}" /proc/net/arp`
-        echo "<host ip=\"${ip_add}\"><hostname>${each}.${TLD}</hostname></host>"  > ${each}.xml
-        virsh net-update ${NET} add dns-host --xml ${each}.xml  --live --config
+        for mac in $mac_add
+        do
+                ip_add=`awk "/${mac}/ {print \\$1}" /proc/net/arp`
+                echo $ip_add
+                echo $mac
+                echo "<host ip=\"${ip_add}\"><hostname>${each}.${TLD}</hostname></host>"  > ${each}${mac}.xml
+                for network in $NET
+                do
+                        virsh net-update $network add dns-host --xml ${each}${mac}.xml  --live --config
+                done
+        done
 done
