bda1aa9ca2995223dac97e75306dd2baf2ea3520
raghavendra talur <raghavendra.talur@gmail.com>
2018-07-18 13:31:23 -0400

    enhance virt-destroy
    Signed-off-by: raghavendra talur <raghavendra.talur@gmail.com>

--- virt-destroy.sh
+++ virt-destroy.sh
@@ -1,1 +1,17 @@
-for each in `virsh list | grep cluster2 | awk '{print $2}'`; do echo  $each; virsh destroy $each; virsh undefine --remove-all-storage $each ; done
+if [[ -z $1 ]]
+then
+        echo "need vm name pattern"
+        exit 1
+fi
+vm_name_pattern=$1
+virsh list --name --all | grep "$vm_name_pattern" | while read each;
+do
+        echo  domain "$each";
+        virsh snapshot-list --roots --name ${each} | while read snaps;
+        do
+                echo $snaps ;
+                virsh snapshot-delete $each --children --snapshotname $snaps ;
+	done
+        virsh destroy $each;
+        virsh undefine --remove-all-storage $each;
+done
